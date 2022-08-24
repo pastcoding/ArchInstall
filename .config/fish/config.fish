a13b08dcbaff26f87239359a9d8c96ff487c529c
Patrick Stromberger <patrick.stromberger@gmail.com>
2022-08-24 17:11:23 +0200

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -100,7 +100,7 @@ alias jctl='journalctl -p 3 -xb'
 ########################################################################################
 
 function commits
-    git log --author="$argv" --format=format:%ad --date=short | uniq -c | awk '{print $1}' | spark | lolcat
+    git log --author="$argv" --format=format:%ad --date=short | uniq -c | awk '{print $1}' 
 end
 
 # Functions needed for !! and !$
